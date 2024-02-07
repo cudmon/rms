6b1f262a8051ce68bc179ec487ecc4de4302b47f
Tonsak <tonsak.gs@gmail.com>
2024-02-07 04:48:52 +0000

    Use `PartialType`

--- packages/api/src/app/menu/menu.dto.ts
+++ packages/api/src/app/menu/menu.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, PartialType } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, Max, Min } from "class-validator";
 
 export class CreateMenuDto {
@@ -20,21 +20,4 @@ export class CreateMenuDto {
   price: number;
 }
 
-export class UpdateMenuDto {
-  @Min(3)
-  @Max(255)
-  @IsNotEmpty()
-  @ApiProperty({ required: true, example: "Burger" })
-  name?: string;
-
-  @Min(1)
-  @Max(255)
-  @IsNotEmpty()
-  @ApiProperty({ required: true, example: "https://example.com/image.jpg" })
-  image?: string;
-
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty({ required: true, example: 10.99 })
-  price?: number;
-}
+export class UpdateMenuDto extends PartialType(CreateMenuDto) {}
