65279c0fd2559f99060d6b614f219a2881c800e3
Tonsak <tonsak.gs@gmail.com>
2024-02-07 05:08:08 +0000

    Update schema

--- packages/api/src/app/menu/menu.dto.ts
+++ packages/api/src/app/menu/menu.dto.ts
@@ -1,19 +1,31 @@
 import { ApiProperty, PartialType } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber, Max, Min } from "class-validator";
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from "class-validator";
 
 export class CreateMenuDto {
-  @Min(3)
-  @Max(255)
+  @MinLength(3)
+  @MaxLength(255)
+  @IsString()
   @IsNotEmpty()
   @ApiProperty({ required: true, example: "Burger" })
   name: string;
 
-  @Min(1)
-  @Max(255)
+  @MinLength(3)
+  @MaxLength(255)
+  @IsString()
   @IsNotEmpty()
   @ApiProperty({ required: true, example: "https://example.com/image.jpg" })
   image: string;
 
+  @Min(1)
+  @Max(1000)
   @IsNumber()
   @IsNotEmpty()
   @ApiProperty({ required: true, example: 10.99 })
