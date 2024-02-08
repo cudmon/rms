a667b9257d2b299bba48bfc55ca65f0ef76a01ac
Tonsak <tonsak.gs@gmail.com>
2024-02-08 04:01:22 +0000

    Add `mantine`

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,13 +1,30 @@
 import { ReactNode } from "react";
+import { ColorSchemeScript, createTheme, MantineProvider } from "@mantine/core";
+
+import "@mantine/core/styles.css";
 
 type Props = {
   children: ReactNode;
 };
 
+const theme = createTheme({
+  primaryColor: "blue",
+  defaultRadius: "md",
+  fontFamily: "Jost, sans-serif",
+  headings: {
+    fontFamily: "Jost, sans-serif",
+  },
+});
+
 export default function Layout({ children }: Props) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
