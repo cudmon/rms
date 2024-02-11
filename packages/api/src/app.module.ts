0672018cd7fb874e06a2a85863c233fdbab2de7f
Tonsak <tonsak.gs@gmail.com>
2024-02-11 13:17:33 +0000

    Add static support

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
 import { MenuModule } from "@/app/menu/menu.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-  imports: [MenuModule],
+  imports: [
+    MenuModule,
+    ServeStaticModule.forRoot({
+      rootPath: `${__dirname}/../uploads`,
+      serveRoot: "/contents",
+    }),
+  ],
 })
 export class AppModule {}
