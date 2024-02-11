888a5bd569c79c1b9771fd846ec8590d3a99986e
Tonsak <tonsak.gs@gmail.com>
2024-02-11 13:49:39 +0000

    Add file validation

--- packages/api/src/app/menu/menu.module.ts
+++ packages/api/src/app/menu/menu.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
 import { MenuService } from "@/app/menu/menu.service";
 import { MulterModule } from "@nestjs/platform-express";
 import { PrismaService } from "@/providers/prisma.service";
 import { MenuController } from "@/app/menu/menu.controller";
+import { BadRequestException, Module } from "@nestjs/common";
 
 @Module({
   controllers: [MenuController],
@@ -10,6 +10,19 @@ import { MenuController } from "@/app/menu/menu.controller";
   imports: [
     MulterModule.register({
       dest: "./uploads/menus",
+      limits: {
+        fileSize: 1024 * 1024 * 5,
+      },
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+          return cb(
+            new BadRequestException("Only image files are allowed!"),
+            false
+          );
+        }
+
+        cb(null, true);
+      },
     }),
   ],
 })
