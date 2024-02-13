2fdba9131560d8b0befa178a2c3a427e859cb897
Tonsak <105202480+cudmon@users.noreply.github.com>
2024-02-13 02:48:59 +0000

    Add service

--- packages/api/src/app/reservation/reservation.service.ts
+++ packages/api/src/app/reservation/reservation.service.ts
@@ -1,7 +1,42 @@
+import { Prisma } from "@prisma/client";
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "@/providers/prisma.service";
 
 @Injectable()
 export class ReservationService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(args: Prisma.ReservationFindManyArgs) {
+    return await this.prisma.reservation.findMany(args);
+  }
+
+  async findOne(args: Prisma.ReservationFindUniqueArgs) {
+    return await this.prisma.reservation.findUnique(args);
+  }
+
+  async create({
+    seat,
+    userId,
+    tableId,
+  }: {
+    seat: number;
+    userId: string;
+    tableId: string;
+  }) {
+    return await this.prisma.reservation.create({
+      data: {
+        seat,
+        userId,
+        tableId,
+      },
+    });
+  }
+
+  async update(args: Prisma.ReservationUpdateArgs) {
+    return await this.prisma.reservation.update(args);
+  }
+
+  async delete(args: Prisma.ReservationDeleteArgs) {
+    return await this.prisma.reservation.delete(args);
+  }
 }
