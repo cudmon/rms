bc809d04ca10d9ef198ed64daf590075c0c13023
Nithit <nithit321@gmail.com>
2024-02-17 17:04:34 +0000

    Add auth module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { MenuModule } from "@/app/menu/menu.module";
+import { UserModule } from "./app/user/user.module";
+import { AuthModule } from "./app/auth/auth.module";
 
 @Module({
-  imports: [MenuModule],
+  imports: [MenuModule, UserModule, AuthModule],
 })
 export class AppModule {}
---
+++ packages/api/src/app/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { compare, hash } from "bcrypt";
+import { Prisma } from "@prisma/client";
+import { JwtService } from "@nestjs/jwt";
+import { CreateUserDto } from "../user/user.dto";
+import { PrismaService } from "@/providers/prisma.service";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService, private prisma: PrismaService) {}
+
+  async logIn(username: string, pass: string): Promise<any> {
+    const user = await this.findOne({ where: { username } });
+    const match = await compare(pass, user.password);
+    if (!match) {
+      return false;
+    }
+    const payload = { id: user.id, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async register({
+    data,
+    select,
+  }: {
+    data: CreateUserDto;
+    select: Prisma.UserSelect;
+  }) {
+    try {
+      return await this.prisma.user.create({
+        data: {
+          ...data,
+          password: await hash(data.password, 10),
+        },
+        select,
+      });
+    } catch (e) {
+    }
+  }
+
+  async findOne({ where, select }: Prisma.UserFindUniqueArgs) {
+    return await this.prisma.user.findUnique({ where, select });
+  }
+}
