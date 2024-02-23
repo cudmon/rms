d68e057c900636ec11933a3f7b859ab946508750
Tonsak <tonsak.gs@gmail.com>
2024-02-23 16:14:21 +0700

    Change env

--- packages/api/src/app/auth/auth.guard.ts
+++ packages/api/src/app/auth/auth.guard.ts
@@ -35,7 +35,7 @@ export class AuthGuard implements CanActivate {
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_SECRET,
+        secret: process.env.SECRET,
       });
 
       request["user"] = payload;
