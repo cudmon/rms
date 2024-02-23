ba148e3476be7c291e07ed2221a8d7fa47f9b8cb
Tonsak <tonsak.gs@gmail.com>
2024-02-23 15:19:13 +0700

    Remove rewrite

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const API_URL = process.env.API_URL;
-
 const nextConfig = {
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${API_URL}/:path*`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
