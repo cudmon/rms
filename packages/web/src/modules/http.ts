aeb17deab6363d08ae224000b4677b494a4f5320
Tonsak <tonsak.gs@gmail.com>
2024-02-25 12:51:55 +0000

    Change api port

--- packages/web/src/modules/http.ts
+++ packages/web/src/modules/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DOMAIN = process.env.DOMAIN || "127.0.0.1:3000";
+const DOMAIN = process.env.DOMAIN || "127.0.0.1:5000";
 
 export const http = axios.create({
   withCredentials: true,
