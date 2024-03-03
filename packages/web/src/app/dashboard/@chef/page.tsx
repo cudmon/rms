2b7c6fa7b53ca7a1a0d6e15e65e7a9a3232de8b0
Tonsak <tonsak.gs@gmail.com>
2024-03-03 05:19:30 +0000

    Add token

--- packages/web/src/app/dashboard/@chef/page.tsx
+++ packages/web/src/app/dashboard/@chef/page.tsx
@@ -1,6 +1,7 @@
-import { Center } from "@mantine/core";
 import { http } from "@/modules/http";
+import { Center } from "@mantine/core";
 import { Order } from "@/types/entity";
+import { cookies } from "next/headers";
 import { ChefOrder } from "@/components/Dashboard/ChefOrder";
 
 export const metadata = {
@@ -11,7 +12,9 @@ export default async function Page() {
   let order: Order[] = [];
 
   try {
-    const res = await http().get("/orders/status/PENDING");
+    const res = await http(cookies().get("token")?.value).get(
+      "/orders/status/PENDING"
+    );
 
     order = res.data;
   } catch (e) {
