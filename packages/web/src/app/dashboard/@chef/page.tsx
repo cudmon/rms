d413381c6b90e05bdadd9a04e53f16bd73ff88c0
Ohmmy <ohmmy569@gmail.com>
2024-03-07 10:25:46 +0000

    Chef finish all

--- packages/web/src/app/dashboard/@chef/page.tsx
+++ packages/web/src/app/dashboard/@chef/page.tsx
@@ -2,32 +2,17 @@ import { http } from "@/modules/http";
 import { Center } from "@mantine/core";
 import { Order } from "@/types/entity";
 import { cookies } from "next/headers";
-import { ChefOrder } from "@/components/Dashboard/ChefOrder";
+import { ChefOrder } from "@/components/Dashboard/Chef/ChefOrder";
 
 export const metadata = {
   title: "Chef",
 };
 
 export default async function Page() {
-  let order: Order[] = [];
-
-  try {
-    const res = await http(cookies().get("token")?.value).get(
-      "/orders/status/PENDING"
-    );
-
-    order = res.data;
-  } catch (e) {
-    return (
-      <Center py={64} fz={28} c="red" fw={500}>
-        Something went wrong. Please try again later
-      </Center>
-    );
-  }
 
   return (
     <div>
-      <ChefOrder orders={order} />
+      <ChefOrder />
     </div>
   );
 }
--- packages/web/src/components/Dashboard/Chef/ChefOrder.tsx
+++ packages/web/src/components/Dashboard/Chef/ChefOrder.tsx
@@ -4,6 +4,9 @@ import { http } from "@/modules/http";
 import { Order } from "@/types/entity";
 import { modals } from "@mantine/modals";
 import { IconX, IconCheck } from "@tabler/icons-react";
+import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { notifications } from "@mantine/notifications";
 import {
   Badge,
   Card,
@@ -14,11 +17,69 @@ import {
   Text,
   ActionIcon,
   Tooltip,
+  Title,
 } from "@mantine/core";
+import { useState, useEffect } from "react";
 
-export const ChefOrder = ({ orders }: { orders: Order[] }) => {
+export const ChefOrder = () => {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  const { isError, data } = useQuery({
+    queryKey: ["orders", "PENDING"],
+    queryFn: async () => {
+      try {
+        const res = await http().get("/orders/status/PENDING");
+
+        return res.data as Order[];
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 404) {
+            return [];
+          }
+        }
+
+        notifications.show({
+          title: "Error",
+          message: "Something went wrong. Please try again later",
+          color: "red",
+        });
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (data !== undefined) {
+      setOrders(data);
+    }
+  }, [data]);
+
+  if (isError) {
+    return (
+      <Center py={64} fz={28} c="red" fw={500}>
+        Error fetching orders
+      </Center>
+    );
+  }
+
+  if (data === undefined) {
+    return (
+      <Center py={64} fz={28} c="red" fw={500}>
+        Loading...
+      </Center>
+    );
+  }
+
   const finish = async (id: string) => {
-    await http().patch(`/orders/finish/${id}`);
+    const res = await http().patch(`/orders/finish/${id}`);
+    if (res.status === 200) {
+      notifications.show({
+        title: "Success",
+        message: "Order finished successfully",
+        color: "green",
+      });
+
+      setOrders(orders.filter((order) => order.id !== id));
+    }
   };
 
   const cancel = (id: string) => {
@@ -43,18 +104,19 @@ export const ChefOrder = ({ orders }: { orders: Order[] }) => {
       children: <Text>Are you sure you want to cancel this order?</Text>,
 
       onConfirm: async () => {
-        await http().patch(`/orders/cancel/${id}`);
+        const res = await http().patch(`/orders/cancel/${id}`);
+        if (res.status === 200) {
+          notifications.show({
+            title: "Success",
+            message: "Order cancelled successfully",
+            color: "green",
+          });
+
+          setOrders(orders.filter((order) => order.id !== id));
+        }
       },
     });
   };
-
-  if (!orders.length) {
-    return (
-      <Center py={64} fz={28} c="red" fw={500}>
-        No order available
-      </Center>
-    );
-  }
 
   const row = orders.map((order) => (
     <Table.Tr key={order.id} ta="center">
@@ -104,6 +166,10 @@ export const ChefOrder = ({ orders }: { orders: Order[] }) => {
 
   return (
     <Container>
+      <Title order={3} size="h2" fw={900} ta="start" c="black" my="md">
+        Chef Order
+      </Title>
+
       <Card shadow="sm" padding="md" radius="md" withBorder mt="sm">
         <Table stickyHeader highlightOnHover verticalSpacing="sm">
           <Table.Thead>{head}</Table.Thead>
