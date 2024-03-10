633634eea53639d636ef2ff584bea9f56bc3d2bf
TNs910 <thanop.s910@gmail.com>
2024-03-10 10:25:36 +0000

    Staff Ui

--- packages/web/src/app/dashboard/@staff/page.tsx
+++ packages/web/src/app/dashboard/@staff/page.tsx
@@ -1,11 +1,33 @@
+import { http } from "@/modules/http";
+import { Center } from "@mantine/core";
+import { cookies } from "next/headers";
+import { TableEntity,Order } from "@/types/entity";
+import { TableStaff } from "@/components/Dashboard/Staff/StaffTable";
+
 export const metadata = {
-  title: "Home",
+  title: "Staff",
 };
 
-export default function Page() {
+export default async function Page() {
+  let TableList: TableEntity[] = [];
+  let orders: Order[] = [];
+  try {
+    const resTable = await http(cookies().get("token")?.value).get("/tables");
+    const resOrder = await http(cookies().get("token")?.value).get("/orders")
+    TableList = resTable.data;
+    orders = resOrder.data;
+  } catch (e) {
+    return (
+      <Center py={64} fz={28} c="red" fw={500}>
+        Something went wrong. Please try again later
+      </Center>
+    );
+  }
+
   return (
-    <>
-      <div>Hello Staff!</div>
-    </>
+    <div>
+      <TableStaff table={TableList} order={orders} />
+
+    </div>
   );
 }
