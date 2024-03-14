95a40ea835fcd8e16801d399bc0d07ac9dbda006
Nithit <nithit321@gmail.com>
2024-03-14 15:33:08 +0000

    Make reservation

--- packages/web/src/app/dashboard/@customer/page.tsx
+++ packages/web/src/app/dashboard/@customer/page.tsx
@@ -1,7 +1,3 @@
-import { http } from "@/modules/http";
-import { Center } from "@mantine/core";
-import { cookies } from "next/headers";
-import { TableEntity } from "@/types/entity";
 import { ReservationList } from "@/components/Dashboard/Customer/ReservationList";
 
 export const metadata = {
@@ -9,23 +5,9 @@ export const metadata = {
 };
 
 export default async function Page() {
-  let TableList: TableEntity[] = [];
-
-  try {
-    const res = await http(cookies().get("token")?.value).get("/tables");
-
-    TableList = res.data;
-  } catch (e) {
     return (
-      <Center py={64} fz={28} c="red" fw={500}>
-        Something went wrong. Please try again later
-      </Center>
-    );
-  }
-
-  return (
     <div>
-      <ReservationList table={TableList} />
+      <ReservationList />
     </div>
   );
 }
--- packages/web/src/components/Dashboard/Customer/ReservationList.tsx
+++ packages/web/src/components/Dashboard/Customer/ReservationList.tsx
@@ -13,6 +13,12 @@ import {
   rem,
   Title,
 } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { http } from "@/modules/http";
+import { AxiosError } from "axios";
+import { modals } from "@mantine/modals";
+import { notifications } from "@mantine/notifications";
+import { useUserStore } from "@/store/user";
 
 const getStatusColor = (status: string) => {
   switch (status) {
@@ -25,10 +31,94 @@ const getStatusColor = (status: string) => {
   }
 };
 
-export const ReservationList = ({ table }: { table: TableEntity[] }) => {
+export const ReservationList = () => {
+  const { user } = useUserStore();
+  const [tableList, setTableList] = useState<TableEntity[]>([]);
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const res = await http().get("/tables");
+        setTableList(res.data);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 404) {
+            setTableList([]);
+            return;
+          }
+        }
+      }
+    };
+    fetch();
+  }, []);
+
+  const confirm = (table: { name: string; id: string }) => {
+    modals.openConfirmModal({
+      title: "Please confirm your action",
+      children: <Text size="sm">Want to reserve?</Text>,
+      labels: { confirm: "Confirm", cancel: "Cancel" },
+      onCancel: () => {},
+      onConfirm: () => {
+        reserve(table.id);
+      },
+    });
+  };
+
+  const reserve = async (tableID: string) => {
+    let currentTime = new Date().toISOString();
+    try {
+      await http().post("/reservations", {
+        when: currentTime,
+        userId: user.id,
+        tableId: tableID,
+      });
+      
+      notifications.show({
+        title: "Reservation success",
+        message: `Table is reserved`,
+      });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response?.data.message === "Table not available") {
+          notifications.show({
+            color: "red",
+            title: "Reservation fail",
+            message: error.response?.data.message,
+          });
+        }
+        if (error.response?.data.message === "Reservation must be made at least 24 hour in advance") {
+          notifications.show({
+            color: "red",
+            title: "Reservation fail",
+            message: error.response?.data.message,
+          });
+        }
+        if (error.response?.data.message === "Reservation must be made at most 30 days in advance") {
+          notifications.show({
+            color: "red",
+            title: "Reservation fail",
+            message: error.response?.data.message,
+          });
+        }
+        if (error.response?.data.message === "You can only have 1 reservation at a time") {
+          notifications.show({
+            color: "red",
+            title: "Reservation fail",
+            message: error.response?.data.message,
+          });
+        }
+      } else {
+        notifications.show({
+          color: "red",
+          title: "Reservation fail",
+          message: `Something went wrong please try again later`,
+        });
+      }
+    }
+  };
+
   return (
     <Container>
-      <Title order={3} size="h1" fw={900} ta="center" >
+      <Title order={3} size="h1" fw={900} ta="center">
         Reservation
       </Title>
       <Text ta="center" mt="md" fw={750} mb={10}>
@@ -36,7 +126,7 @@ export const ReservationList = ({ table }: { table: TableEntity[] }) => {
       </Text>
 
       <Grid>
-        {table.map((table) => (
+        {tableList.map((table) => (
           <Grid.Col span={4} key={table.id}>
             <Card shadow="sm" padding="lg" radius="md" withBorder>
               <Grid columns={2} justify="flex-start" align="center">
@@ -67,7 +157,15 @@ export const ReservationList = ({ table }: { table: TableEntity[] }) => {
 
                 <Grid.Col span={1}>
                   <center>
-                    <Button variant="filled" color="blue.5" radius="md">
+                    <Button
+                      disabled={table.status === "IDLE" ? false : true}
+                      onClick={() =>
+                        confirm({ name: table.name, id: table.id })
+                      }
+                      variant="filled"
+                      color={table.status === "IDLE" ? "blue.5" : "red.5"}
+                      radius="md"
+                    >
                       Reserve
                     </Button>
                   </center>
