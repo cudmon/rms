9f7181b4e2c9d1603ee2165ed42f6be048cc2886
Tonsak <tonsak.gs@gmail.com>
2024-03-14 13:51:32 +0000

    Remove rsc api call

--- packages/web/src/app/dashboard/@manager/menus/page.tsx
+++ packages/web/src/app/dashboard/@manager/menus/page.tsx
@@ -1,7 +1,3 @@
-import { http } from "@/modules/http";
-import { Menu } from "@/types/entity";
-import { Center } from "@mantine/core";
-import { cookies } from "next/headers";
 import { ManagerMenus } from "@/components/Dashboard/Manager/ManagerMenus";
 
 export const metadata = {
@@ -9,21 +5,9 @@ export const metadata = {
 };
 
 export default async function Page() {
-  let menu: Menu[] = [];
-  try {
-    const res = await http(cookies().get("token")?.value).get("/menus");
-    menu = res.data;
-  } catch (e) {
-    return (
-      <Center py={64} fz={28} c="red" fw={500}>
-        Something went wrong. Please try again later
-      </Center>
-    );
-  }
-
   return (
     <div>
-      <ManagerMenus food={menu} />
+      <ManagerMenus />
     </div>
   );
 }
