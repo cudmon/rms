062f375fd552829c06e261cbe6b0be13575d8734
Tonsak <tonsak.gs@gmail.com>
2024-03-16 14:05:45 +0000

    Proxy API

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,6 +2,15 @@
 
 const nextConfig = {
   reactStrictMode: true,
+
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
